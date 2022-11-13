8a92c4fe35c2833fa714eea92352535b97746b04
Goodnessmbakara <amicablembakara50@gmail.com>
2022-11-13 14:47:05 -0800

    Hot

--- 0x05-python-exceptions/5-raise_exception.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# 5-raise_exception.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
-
-
-def raise_exception():
-    """Raise a TypeError exception."""
-    raise TypeError
--- 0x06-python-classes/0-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 0-square.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
-"""Define a class Square."""
-
-
-class Square:
-    """Represent a square."""
-    pass
